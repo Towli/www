9af60d1fc9188d8f746ff72b749f4bd554abaa01
Alex Towli <alextowli@hotmail.co.uk>
2020-06-19 19:57:30 +0100

    feat(portfolio): add tps project and extra info

--- src/js/interactions.js
+++ src/js/interactions.js
@@ -1,27 +1,37 @@
-$(document).ready(function() {
-  var profileContainer = $('.profile');
-  var portfolioContainer = $('.portfolio');
-  var contactContainer = $('.contact');
-  var portfolioLink = $('#portfolio-link');
-  var contactLink = $('#contact-link');
-  var backLink = $('.back-link');
+$(document).ready(function () {
+  var profileContainer = $(".profile");
+  var portfolioContainer = $(".portfolio");
+  var contactContainer = $(".contact");
+  var portfolioLink = $("#portfolio-link");
+  var contactLink = $("#contact-link");
+  var backLink = $(".back-link");
+  var workThumbnail = $(".work-thumbnail-container");
 
-  portfolioLink.click(function() {
+  portfolioLink.click(function () {
     profileContainer.removeClass("visible").addClass("hidden");
     contactContainer.removeClass("visible").addClass("hidden");
     portfolioContainer.removeClass("hidden").addClass("visible");
   });
 
-  contactLink.click(function() {
+  contactLink.click(function () {
     profileContainer.removeClass("visible").addClass("hidden");
     portfolioContainer.removeClass("visible").addClass("hidden");
     contactContainer.removeClass("hidden").addClass("visible");
   });
 
-  backLink.click(function() {
+  backLink.click(function () {
     portfolioContainer.removeClass("visible").addClass("hidden");
     contactContainer.removeClass("visible").addClass("hidden");
     profileContainer.removeClass("hidden").addClass("visible");
   });
 
+  workThumbnail.hover(
+    function () {
+      console.log("hover");
+      $(this).children().eq(1).removeClass("hidden");
+    },
+    function () {
+      $(this).children().eq(1).addClass("hidden");
+    }
+  );
 });
