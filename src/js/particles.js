80b585e281e0cb72c28ae66ebc6f9e2fabd01aac
towli <alextowli@hotmail.co.uk>
2023-03-27 23:13:45 +0100

    feat: refactor and stylise particles

--- src/js/particles.js
+++ src/js/particles.js
@@ -1,50 +1,58 @@
-var w = window.innerWidth;
-var h = window.innerHeight;
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-var rate = 50;
-var numParticles = 200;
-var particles = [];
-var time,
-    count,
-    size = 2,
-    speed = 10,
-    lights = new Array,
-    colors = ['#000'];
+'use strict';
 
-canvas.setAttribute('width',w);
-canvas.setAttribute('height',h);
+const colours = ['#FBF8CC', '#FDE4CF', '#FFCFD2', '#F1C0E8', '#CFBAF0', '#A3C4F3', '#90DBF4', '#8EECF5', '#98F5E1', '#B9FBC0'];
+const w = window.innerWidth;
+const h = window.innerHeight;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const rate = 50;
+const numParticles = 55;
+const particles = [];
+let time,
+  count,
+  size = 30,
+  speed = 10,
+  lights = new Array,
+  colors = ['#000'];
+
+canvas.setAttribute('width', w);
+canvas.setAttribute('height', h);
 
 for (var i = 0; i < numParticles; i++) {
   particles.push(new createParticle());
 }
 
-// Randomise creation and placement of particle
+function getRandomColour() {
+  return colours[Math.round(Math.random() * colours.length) - 1];
+}
+
 function createParticle() {
-  this.x = Math.random()*w;
-  this.y = Math.random()*h;
-  this.velocityX = (Math.random()*2)-1;
-  this.velocityY = (Math.random()*2)-1;
-  this.radius = Math.random()*1.5;
-  this.color = "black";
+  this.x = Math.random() * w;
+  this.y = Math.random() * h;
+  this.velocityX = (Math.random() * 2) - 1;
+  this.velocityY = (Math.random() * 2) - 1;
+  this.radius = Math.random() * size;
+  this.color = getRandomColour();
+  console.log(this.color);
 }
 
 function draw() {
-  context.clearRect(0,0,w,h);
+  context.clearRect(0, 0, w, h);
 
-  for(var i = 0; i < particles.length; i++) {
+  particles.forEach(particle => {
     context.beginPath();
-    context.arc(particles[i].x, particles[i].y, particles[i].radius, 0, 2*Math.PI);
-    context.fillStyle = 'rgba(0, 0, 0, 0.8)';
+    context.arc(particle.x, particle.y, particle.radius, 0, 2 * Math.PI);
+    context.fillStyle = particle.color;
+    context.globalAlpha = 0.5;
     context.fill();
 
-    particles[i].x += particles[i].velocityX;
-    particles[i].y += particles[i].velocityY;
+    particle.x += particle.velocityX;
+    particle.y += particle.velocityY;
 
-    if (particles[i].x >= w || particles[i].x <= 0)
-      particles[i].velocityX = -particles[i].velocityX;
-    if (particles[i].y >= h || particles[i].y <= 0)
-      particles[i].velocityY = -particles[i].velocityY;
-  }
+    if (particle.x >= w || particle.x <= 0)
+      particle.velocityX = -particle.velocityX;
+    if (particle.y >= h || particle.y <= 0)
+      particle.velocityY = -particle.velocityY;
+  });
 }
 setInterval(draw, 33);
